fbbccd395878b381f661e992ab5d5a731a1b1dc3
Souad ourak <souad.ourak@gmail.com>
2020-01-09 10:23:34 -0500

    Créer les endpoint de product

---
+++ api/routes/products.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+// GET /products
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling GET requests to /products'
+  });
+});
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling POST requests to /products'
+  });
+});
+
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  if (id === 'special') {
+    res.status(200).json({
+      message: 'You discovered the special ID'
+    });
+  }
+});
+
+router.patch('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Updated product'
+  });
+});
+
+router.delete('/:productId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Deleted product'
+  });
+});
+module.exports = router;
--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
+const productRoutes = require('./api/routes/products');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.status(200).json({ message: 'It works' });
-});
+app.use('/products', productRoutes);
 
 app.listen(port);
