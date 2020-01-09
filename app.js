072e6c911c433526f0ce06cf4470c319e3d6388a
Souad ourak <souad.ourak@gmail.com>
2020-01-09 13:23:59 -0500

    Ajouter  bodyparser

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-headers',
+    'Origin, X-Request-With, Content-Type, Accept, Authorization'
+  );
+  res.header('Access-Control-Allow-Method', 'GET, PUT, POST, DELETE, PATCH');
+  next();
+});
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
@@ -8,6 +19,8 @@ const orderRoutes = require('./api/routes/orders');
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
