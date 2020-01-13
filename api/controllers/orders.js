3dd9305c0963c984e4a647abfa8539849f8bb1dc
Souad ourak <souad.ourak@gmail.com>
2020-01-13 10:25:18 -0500

    Ajouter Order

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -1,31 +1,110 @@
+const Order = require('../models/orders');
+const Product = require('../models/products');
+
 // Hnadling incoming GET requests to /orders
 exports.getOrders = (req, res, next) => {
-  res.status(200).json({
-    message: 'Orders were fetched'
-  });
+  Order.find()
+    .select('product quantity _id')
+    .populate('product')
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map(doc => {
+          return {
+            _id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+            request: {
+              type: 'GET',
+              url: 'http://localhost:3000/orders/' + doc._id
+            }
+          };
+        })
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      json.status(500).json({ error: err });
+    });
 };
 
 // Handling incoming POST requests to /orders
 exports.postOrder = (req, res, next) => {
-  res.status(201).json({
-    message: 'Order was created'
-  });
+  Product.findById(req.body.productId)
+    .then(product => {
+      if (!product) {
+        return res.status(404).json({
+          message: 'Product not found'
+        });
+      }
+
+      const order = new Order({
+        quantity: req.body.quantity,
+        product: req.body.productId
+      });
+      order.save().then(result => {
+        res.status(201).json({
+          massage: 'Order stored',
+          creactedOrder: {
+            _id: result._id,
+            product: result.product,
+            quantity: result.quantity
+          },
+          request: {
+            Type: 'POST',
+            url: 'http://localhost:3000/orders'
+          }
+        });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 };
 
 // Handling incoming GET requests to /orders/:orderId
 exports.getOrder = (req, res, next) => {
-  const orderId = req.params.orderId;
-
-  res.status(200).json({
-    message: 'You discovered a special order'
-  });
+  Order.findById(req.params.orderId)
+    .select('product quantity _id')
+    .populate('product')
+    .exec()
+    .then(order => {
+      if (!order) {
+        return res.status(404).json({
+          message: 'Order not found'
+        });
+      }
+      res.status(200).json({
+        order: order,
+        request: {
+          type: 'GET',
+          url: 'http://localhost:3000/orders'
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 };
 
 // Handling incoming DELETE requests to /orders/:orderId
 exports.deleteOrder = (req, res, next) => {
-  const orderId = req.params.orderId;
-
-  res.status(200).json({
-    message: 'Order was deleted'
-  });
+  Order.deleteOne({ _id: req.params.orderId })
+    .exec()
+    .then(result => {
+      res.status(200).json({
+        message: 'Order deleted',
+        request: {
+          type: 'POST',
+          url: 'http://localhost:3000/orders'
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 };
--- api/controllers/products.js
+++ api/controllers/products.js
@@ -1,37 +1,108 @@
+const Product = require('../models/products');
+
 // Handling GET requests to /products
 exports.getProducts = (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET requests to /products'
-  });
+  Product.find()
+    .then(docs => {
+      const response = {
+        count: docs.length,
+        products: docs.map(doc => {
+          return {
+            name: doc.name,
+            price: doc.price,
+            _id: doc._id,
+            request: {
+              type: req.method,
+              url: 'http://localhost:3000/products/' + doc._id
+            }
+          };
+        })
+      };
+
+      res.status(200).json(response);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
 };
 
 // Handling POST requests to /products
 exports.createProduct = (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling POST requests to /products'
+  const product = new Product({
+    name: req.body.name,
+    price: req.body.price
   });
+
+  product
+    .save()
+    .then(result => {
+      res.status(200).json({
+        product: {
+          name: result.name,
+          price: result.price,
+          _id: result._id,
+          request: {
+            type: req.method,
+            url: 'http://localhost:3000/products/' + result._id
+          }
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 };
 
 // Handling GET requests to /products/:productId
 exports.getProduct = (req, res, next) => {
   const id = req.params.productId;
-  if (id === 'special') {
-    res.status(200).json({
-      message: 'You discovered the special ID'
+
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({ message: 'Aucune valeur pour le ID fournis' });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
     });
-  }
 };
 
 // Handling PATCH requests to /products/:productId
 exports.updateProduct = (req, res, next) => {
-  res.status(200).json({
-    message: 'Updated product'
-  });
+  const id = req.params.productId;
+  Product.update(
+    { _id: id },
+    { $set: { name: req.body.name, price: req.body.price } }
+  )
+    .exec()
+    .then(result => {
+      res.status(200).json({ message: 'Product updated' });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 };
 
 // Handling DELETE requests to /products/:productId
 exports.deleteProduct = (req, res, next) => {
-  res.status(200).json({
-    message: 'Deleted product'
-  });
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json({ message: 'Product deleted' });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 };
